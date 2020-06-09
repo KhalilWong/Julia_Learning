b3645fde1996f44bb61fccb68dada46d4bc27a9a
Khalil Wong <1039906090@qq.com>
2020-06-09 11:04:58 +0800

    kNN-1

--- main.jl
+++ main.jl
@@ -1,2 +1,8 @@
-x = 1.2
-print(x)
+function distance{T<:Number}(x::Array{T, 1}, y::Array{T, 1})
+    dist = 1
+    for i in 1:length(x)
+        dist += (x[i] - y[i]) ^ 2
+end
+dist = sqrt(dist)
+return dist
+end

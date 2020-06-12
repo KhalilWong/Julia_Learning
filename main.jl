3ac185176bbdd0c77f7de33d7eea8315960b1787
Khalil Wong <1039906090@qq.com>
2020-06-12 11:03:03 +0800

    Simple kNN Done!

--- main.jl
+++ main.jl
@@ -2,7 +2,7 @@ using Random
 
 ################################################################################
 function distance(x::Array{T, 1}, y::Array{T, 1}) where {T<:Number}
-    dist = 0
+    dist = 0.0
     for i in 1:length(x)
         dist += (x[i] - y[i]) ^ 2
     end
@@ -13,38 +13,40 @@ end
 function classify(distances::Array{Float64, 1}, labels::Array{T, 1}, k::Int64) where {T<:Any}
     class = unique(labels)                                                      #所有类别不重复
     nc = length(class)                                                          #类别数
-    indexes = Array{Int}(undef, k)                                                     #
+    indexes = Array{Int64}(undef, k)                                            #
     M = typemax(typeof(distances[1]))
-    class_count = Array{Int}(undef, nc)
+    class_count = Array{Int64}(undef, nc)
+    class_count[1] = 0
+    class_count[2] = 0
     for i in 1:k
         d_min, indexes[i] = findmin(distances)
         distances[indexes[i]] = M
     end
     klabels = labels[indexes]
-    for i in 1:nc
-        for j in 1:k
+    for j in 1:k
+        for i in 1:nc
             if klabels[j] == class[i]
                 class_count[i] += 1
-                break
             end
         end
     end
     d_max, index = findmax(class_count)
-    return class[index]
+    return class[index], klabels
 end
 ################################################################################
 function apply_kNN(X::Array{T1, 2}, x::Array{T2, 1}, Y::Array{T1, 2}, k::Int) where {T1<:Number, T2<:Any}
     N = size(X, 1)                                                              #已知数据点
     n = size(Y, 1)                                                              #待分类数据点
-    D = Array{Float64}(undef, N)                                                       #初始化距离矢量（中间计算距离过程）
-    z = Array{typeof(x[1])}(undef, n)                                                  #初始化标签矢量（最终分类结果）
+    D = Array{Float64}(undef, N)                                                #初始化距离矢量（中间计算距离过程）
+    z = Array{typeof(x[1])}(undef, n)                                           #初始化标签矢量（最终分类结果）
+    klabels = Array{Any, 2}(undef, n, k)
     for i in 1:n
         for j in 1:N
             D[j] = distance(X[j, :], Y[i, :])
         end
-        z[i] = classify(D, x, k)
+        z[i], klabels[i, :] = classify(D, x, k)
     end
-    return z
+    return z, klabels
 end
 ################################################################################
 println("\nBeginning...")
@@ -61,6 +63,7 @@ println(FN)
 #println(typeof(FN))
 #
 I = Array{Float64, 2}(undef, N, FN - 1)
+println(I[1, 1])
 O = Array{Any}(undef, N)
 for i in 1:N
     lineString = split(lines[i], ",")
@@ -69,9 +72,7 @@ for i in 1:N
     end
     O[i] = lineString[FN]
 end
-println(typeof(O))
-println(typeof(O[1]))
-n = round(Int64, N / 3 * 2)
+n = round(Int64, N / 2)
 R = randperm(N)                                                                 #随机排列
 indX = R[1:n]
 X = I[indX, :]
@@ -79,11 +80,12 @@ x = O[indX]
 indY = R[(n + 1):end]
 Y = I[indY, :]
 y = O[indY]
-z = apply_kNN(X, x, Y, 5)
+z, klabels = apply_kNN(X, x, Y, 10)
 println(z[1:5])
+println(klabels[1:5, :])
 println(y[1:5])
 println(sum(y[1:5] .== z[1:5]))
-println(sum(y .== z) / n)
+println(sum(y .== z) / (N - n))
 #println(z[1:5])
 #println(z[1:5])
 println("End\n")
